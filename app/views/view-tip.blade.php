eea60944c2c7f49e15b4bd083a22a541c97b97dc
Gonzalo Z Server Lan <gonzunigad@gmail.com>
2014-07-06 21:28:01 -0400

    Mejoras ver tip

--- app/views/view-tip.blade.php
+++ app/views/view-tip.blade.php
@@ -17,7 +17,31 @@
     <h3 class="lugar">nombre del lugar</h3>
     <h4 class="autor">nombre del autor</h4>
     <div class="detalle_city">
-    
+    <h2 class="slide">Imágenes del lugar</h2>
+    <ul>
+    <li><img src="{{ asset('img/detalle1.jpg') }}"/></li>
+    <li><img src="{{ asset('img/detalle2.jpg') }}" /></li>
+    <li><img src="{{ asset('img/detalle3.jpg') }}" /></li>
+    <li><img src="{{ asset('img/detalle1.jpg') }}"/></li>
+    <li><img src="{{ asset('img/detalle2.jpg') }}"/></li>
+    <li><img src="{{ asset('img/detalle3.jpg') }}"/></li>
+    <li><img src="{{ asset('img/detalle1.jpg') }}" /></li>
+    </ul>
+    <h2 class="slide">Mapa del lugar</h2>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3329.8482786191244!2d-70.6038805!3d-33.42719969999999!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cf6e080d5f15%3A0x38cf49fd622be98b!2sCarlos+Ant%C3%BAnez!5e0!3m2!1ses!2scl!4v1404687428294" width="850" height="262" frameborder="0" style="border:0"></iframe>
+    <h2 class="slide">Si estuviste aquí o te gustó el dato califícalo.</h2>
+    <div class="rating">
+				<span class="mark active"></span>
+				<span class="mark active"></span>
+				<span class="mark active"></span>
+				<span class="mark"></span>
+				<span class="mark"></span>
+			</div>
+            <div class="shared">
+            <a class="ruta"></a>
+            <a class="face"></a>
+            <a class="tw"></a>
+            </div>
     </div>
     <p class="intro">¡Todos estos datos son entregados por usuarios como ustedes, somos una verdadera comunidad!</p>
 </div>
