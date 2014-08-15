b16364537d2c4fc621cb51ee0462b4fb9b778f3b
Gonzalo Z <gonzunigad@gmail.com>
2014-08-15 11:56:00 -0400

    PUnto finales en mensaje de gracias

--- app/views/tips/thanks.blade.php
+++ app/views/tips/thanks.blade.php
@@ -6,9 +6,9 @@
     <section class="datos">
         <div id="content-form">
             <div class="thanks">
-                <h1>GRACIAS POR ABRIR TU CORAZÓN Y CONTARNOS ESAS INFIDENCIAS DE TU AMOR POR CHILE</h1>
+                <h1>GRACIAS POR ABRIR TU CORAZÓN Y CONTARNOS ESAS INFIDENCIAS DE TU AMOR POR CHILE.</h1>
 
-                <h1>SIGUE REGANDO ESTE AMOR Y CONTINÚA NAVEGANDO EN ENAMÓRATE DE CHILE</h1>
+                <h1>SIGUE REGANDO ESTE AMOR Y CONTINÚA NAVEGANDO EN ENAMÓRATE DE CHILE.</h1>
 
                 <div class="btn-enviar">
                     <form method="get" action="{{ URL::route('view-tip',$tip->id) }}">
