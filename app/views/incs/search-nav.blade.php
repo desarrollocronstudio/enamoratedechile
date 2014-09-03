7635d38772989cf1faddd67d0f4879bf510bc6e0
Gonzalo Z <gonzunigad@gmail.com>
2014-09-03 13:49:28 -0400

    Al buscar tu dato se abre altiro buscador

--- app/views/incs/search-nav.blade.php
+++ app/views/incs/search-nav.blade.php
@@ -21,7 +21,7 @@
 			//return false;
 		});
 		$(".go-search").click(function(){
-			$(".search-box").fadeIn(500);
+			$(".search-box").fadeIn(500).find("input[type=text]").focus();
 			$('html, body').animate({
                 scrollTop: $("#search-nav").offset().top-50
             }, 800);
