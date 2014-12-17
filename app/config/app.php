9f0998afc87dd3921eee3302518bd1883c2ffe5c
Gonzalo De Spirito <gonzunigad@gmail.com>
2014-12-17 15:54:14 -0300

    Integracion de redes sociales footer + cambio de url en config para los share

--- app/config/app.php
+++ app/config/app.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://www.enamoratedechile.cl',
+	'url' => 'https://ssl.lan.com/enamoratedechile',
 
 	/*
 	|--------------------------------------------------------------------------
--- app/views/incs/footer.blade.php
+++ app/views/incs/footer.blade.php
@@ -2,9 +2,9 @@
     <div class="contenido-footer">
     <span class="copyright">© {{ date("Y") }} LATAM AIRLINES GROUP S.A. - Todos los derechos reservados.</span>
     <span class="social-media">
-        <span class="btn-social"><a href="#"><img src='{{ asset("img/social-media-footer_fb.png") }}' border="0" /></a></span>
-        <span class="btn-social"><a href="#"><img src='{{ asset("img/social-media-footer_tw.png") }}' border="0" /></a></span>
-        <span class="btn-social"><a href="#"><img src='{{ asset("img/social-media-footer_ytb.png") }}' border="0" /></a></span>
+        <span class="btn-social"><a target="_blank" href="http://www.facebook.com/lanenchile"><img src='{{ asset("img/social-media-footer_fb.png") }}' border="0" /></a></span>
+        <span class="btn-social"><a target="_blank" href="http://www.twitter.com/lan_cl"><img src='{{ asset("img/social-media-footer_tw.png") }}' border="0" /></a></span>
+        <span class="btn-social"><a target="_blank" href="http://www.youtube.com/lanenchile "><img src='{{ asset("img/social-media-footer_ytb.png") }}' border="0" /></a></span>
     </span>
     <span class="bases"><a href="{{ asset("terminos.pdf") }}" target="_blank">Términos de uso del sitio</a></span>
     <span class="up-btn desktop"><a href="#"><img src='{{ asset("img/btn-up.png") }}' border="0" /></a></span>
