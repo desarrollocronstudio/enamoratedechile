e8c73a06caa21b3d39e1739d8942136a8a059b6a
Gonzalo De Spirito <gonzunigad@gmail.com>
2014-12-30 18:23:46 -0300

    Eliminar share y solo mostrar tips activos

--- app/controllers/TipController.php
+++ app/controllers/TipController.php
@@ -10,7 +10,7 @@ class TipController extends BaseController {
 		
 		$lat = $position["lat"];
 		$lng = $position["lng"];
-		$distances = [20,80];
+		$distances = [20,50];
 		$minimum_places = 3;
 
 		$tips = [];
