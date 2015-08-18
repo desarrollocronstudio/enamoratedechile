fde1efd20e8b88da23ea7cbccb30acb2157c7c23
Cronstudio <ricardo@cronstudio.com>
2015-08-18 11:57:31 -0300

    test cron title

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>test crons @yield("page_title",'Enamórate de Chile') - LAN</title>
+        <title>@yield("page_title",'Enamórate de Chile') - LAN</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
         <meta name="_token" content="{{ csrf_token() }}" />
